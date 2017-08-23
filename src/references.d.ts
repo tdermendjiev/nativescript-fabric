f92d6356d3143d30c9b1198dd12bacb69035c2d8
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-08-23 18:00:12 +0200

    fix(build-error): Resolve build error

--- src/references.d.ts
+++ src/references.d.ts
@@ -5,5 +5,7 @@ declare class UIApplication { }
 declare class NSDictionary { }
 declare class UIApplicationDelegate { }
 declare interface UIApplicationDelegate { }
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.es2016.d.ts" />
+
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
